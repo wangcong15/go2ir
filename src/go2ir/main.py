97604da723363b559864db4d284bcd74ef96200b
wangcong15 <wangcong15@mails.tsinghua.edu.cn>
2018-09-12 10:52:45 +0800

    init

---
+++ src/go2ir/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+def main():
+    # Define command line arguments
+    arg_parser = argparse.ArgumentParser()
+    print("Hello Golang!")
+
+if __name__ == "__main__":
+    main()
